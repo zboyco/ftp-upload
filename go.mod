914b1287093df7170c8478ab009ea170a65ea014
白羊 <shannon@shannondeMacBook-Pro.local>
2022-11-18 18:37:14 +0800

    feat: 🎸 增加进度条

--- go.mod
+++ go.mod
@@ -5,9 +5,16 @@ go 1.19
 require (
 	github.com/go-ini/ini v1.67.0
 	github.com/jlaffaye/ftp v0.1.0
+	github.com/schollz/progressbar/v3 v3.12.1
+	golang.org/x/exp v0.0.0-20221114191408-850992195362
 )
 
 require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 	"sync"
@@ -11,6 +10,8 @@ import (
 
 	"github.com/go-ini/ini"
 	"github.com/jlaffaye/ftp"
+	"github.com/schollz/progressbar/v3"
+	log "golang.org/x/exp/slog"
 )
 
 // ftp 配置
@@ -24,21 +25,17 @@ type ftpConfig struct {
 }
 
 // ftpSetting App配置
-var ftpSetting = &ftpConfig{}
-
-var cfg *ini.File
-
-var c *ftp.ServerConn
-
-var ch chan *ftpCmd
-
-var totalCount int
-
-var currentCount int
-
-var timeLine time.Time
-
-var wg sync.WaitGroup
+var (
+	ftpSetting   = &ftpConfig{}
+	cfg          *ini.File
+	c            *ftp.ServerConn
+	ch           chan *ftpCmd
+	totalCount   int64
+	currentCount int
+	bar          *progressbar.ProgressBar
+	timeLine     time.Time
+	wg           sync.WaitGroup
+)
 
 type ftpCmd struct {
 	Type      int
@@ -49,56 +46,61 @@ func init() {
 	var err error
 	cfg, err = ini.Load("ftp.ini")
 	if err != nil {
-		fmt.Println("配置加载失败:", err)
+		log.Error("配置加载失败:", err)
 		return
 	}
 	// 将读操作提升大约 50-70% 的性能
 	cfg.BlockMode = false
 
-	cfg.Section("").MapTo(ftpSetting)
+	_ = cfg.Section("").MapTo(ftpSetting)
 	timeLine, _ = time.Parse("2006-01-02 15:04:05", "1975-01-1 00:00:00")
+
 }
 
 func main() {
-	var err error
-	var interval int
-	flag.IntVar(&interval, "s", 300, "上传时间区间（秒），默认上传300秒内修改的文件,为0上传所有文件")
+	var (
+		err      error
+		interval int
+	)
+
+	flag.IntVar(&interval, "s", 300, "上传时间区间(秒), 默认上传300秒内修改的文件,为0上传所有文件")
 	flag.Parse()
 	if interval != 0 {
 		timeLine = time.Now().Add(-time.Duration(interval) * time.Second)
 	}
 	c, err = ftp.Dial(fmt.Sprintf("%s:%d", ftpSetting.Host, ftpSetting.Port), ftp.DialWithTimeout(30*time.Second))
 	if err != nil {
-		fmt.Println("FTP连接失败:", err)
+		log.Error("FTP连接失败:", err)
 		return
 	}
-	fmt.Println("FTP连接成功...")
+	log.Info("FTP连接成功...")
 	err = c.Login(ftpSetting.Username, ftpSetting.Password)
 	if err != nil {
-		fmt.Println("FTP登录失败:", err)
+		log.Error("FTP登录失败:", err)
 		return
 	}
-	fmt.Println("FTP登录成功...")
+	log.Info("FTP登录成功...")
 	wg.Add(1)
 	go work()
 
 	ch = make(chan *ftpCmd, 1000)
-	fmt.Println("上传开始...")
+	log.Info("上传开始...")
+	bar = progressbar.Default(1, "上传进度")
 	err = uploadAllFiles(ftpSetting.LocalPath)
 	close(ch)
 	if err != nil {
-		fmt.Println("\n文件上传失败:", err)
+		log.Error("\n文件上传失败:", err)
 		return
 	}
 
 	wg.Wait()
 
 	if err := c.Quit(); err != nil {
-		fmt.Println("\nFTP登出失败:", err)
+		log.Error("\nFTP登出失败:", err)
 		return
 	}
-	fmt.Println("\nFTP登出成功...")
-	fmt.Println("OK,上传完成...")
+	log.Info("FTP登出成功...")
+	log.Info("OK, 上传完成...")
 }
 
 func work() {
@@ -121,21 +123,23 @@ func work() {
 
 		err = c.Stor(remotePath, data)
 		if err != nil {
-			fmt.Println(cmd.LocalPath)
+			log.Info(cmd.LocalPath)
 			panic(err)
 		}
-		stdPrint()
+		if err := bar.Add(1); err != nil {
+			log.Error("进度错误", err)
+		}
 	}
 }
 
 func makeDir(loachPath string) {
 	remotePath := strings.Replace(loachPath, ftpSetting.LocalPath, ftpSetting.RemotePath, 1)
-	c.MakeDir(remotePath)
+	_ = c.MakeDir(remotePath)
 }
 
 // uploadAllFiles 上传指定目录下的所有文件,包含子目录下的文件
 func uploadAllFiles(dirPth string) error {
-	dirs, err := ioutil.ReadDir(dirPth)
+	dirs, err := os.ReadDir(dirPth)
 	if err != nil {
 		return err
 	}
@@ -144,14 +148,15 @@ func uploadAllFiles(dirPth string) error {
 		LocalPath: dirPth,
 	}
 	pathSep := "/"
-	for _, fi := range dirs {
-		path := fmt.Sprintf("%s%s%s", dirPth, pathSep, fi.Name())
-		if fi.IsDir() { // 目录, 递归遍历
+	for _, dir := range dirs {
+		path := fmt.Sprintf("%s%s%s", dirPth, pathSep, dir.Name())
+		if dir.IsDir() { // 目录, 递归遍历
 			err := uploadAllFiles(path)
 			if err != nil {
 				return err
 			}
 		} else {
+			fi, _ := dir.Info()
 			checkFile(path, fi)
 		}
 	}
@@ -168,9 +173,5 @@ func checkFile(localPath string, file os.FileInfo) {
 		LocalPath: localPath,
 	}
 	totalCount++
-	stdPrint()
-}
-
-func stdPrint() {
-	fmt.Print("\r上传进度:", fmt.Sprintf("%d/%d", currentCount, totalCount))
+	bar.ChangeMax64(totalCount)
 }
